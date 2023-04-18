26f02c82d6f77479d47bb35231d489460eb4e095
Peculiarguru <peculiar4ezeh@gmail.com>
2023-04-18 14:33:12 +0200

    task completed

--- solution.js
+++ solution.js
@@ -1,12 +1,3 @@
-// AttackSkill class
-class AttackSkill {
-  constructor(name, damage, magic) {
-    this.name = name;
-    this.damage = damage;
-    this.magic = magic;
-  }
-}
-
 // Pokemon class
 class Pokemon {
   constructor(name, health, magic) {
@@ -15,64 +6,74 @@ class Pokemon {
     this.magic = magic;
     this.skills = [];
   }
-
+  //   learn skill method
   learnAttackSkill(skill) {
     this.skills.push(skill);
   }
-
+  //   show status method
   showStatus() {
     console.log(`${this.name} status`);
     console.log(`health: ${this.health}`);
     console.log(`magic: ${this.magic}`);
   }
+  //   show status method
 
+  // attack method
   attack(skillIndex, target) {
     const skill = this.skills[skillIndex];
     if (!skill) {
       console.log(`${this.name} does not have that skill!`);
-      return;
-    }
-    if (this.magic < skill.magic) {
-      console.log(
-        `${this.name} does not have enough magic to use ${skill.name}!`
-      );
-      return;
-    }
-    this.magic -= skill.magic;
-    target.health -= skill.damage;
-    console.log(`${this.name} launched skill '${skill.name}' successfully!`);
-    console.log(`${target.name} got ${skill.damage} damage`);
-    if (target.health <= 0) {
-      console.log(`${target.name} is killed!`);
+    } else if (this.magic < skill.amountOfMagic) {
+      console.log(`not enough magic, cannot launch attack!`);
+    } else if (this.health <= 0) {
+      console.log(`${this.name} is already dead!`);
+    } else {
+      if (target.health <= 0) {
+        console.log(`${target.name} is killed!`);
+      } else {
+        this.magic -= skill.amountOfMagic;
+        target.health -= skill.amountOfDamage;
+        console.log(
+          `${this.name} launched skill '${skill.name}' successfully!`
+        );
+        console.log(`${target.name} got ${skill.amountOfDamage} damage`);
+      }
     }
   }
-
+  //   getMagic method
   getMagic() {
-    const restoredMagic = Math.floor(Math.random() * 10) + 1;
-    this.magic += restoredMagic;
-    console.log(`${this.name} got ${restoredMagic} magic back`);
+    const magic = 20;
+    this.magic += magic;
+    console.log(`${this.name} got ${magic} magic back`);
   }
 }
 
-// Example usage
+// AttackSkill class
+class AttackSkill {
+  constructor(name, amountOfDamage, amountOfMagic) {
+    this.name = name;
+    this.amountOfDamage = amountOfDamage;
+    this.amountOfMagic = amountOfMagic;
+  }
+}
 let pikachu = new Pokemon("pikachu", 120, 80);
 let bulbasaur = new Pokemon("bulbasaur", 95, 105);
 
+//Each skill should do a certain amount of damage, and consume a certain amount of magic from the Pokemon that used the skill.
 let lightning = new AttackSkill("lightning", 40, 30);
 let poisonSeed = new AttackSkill("poison seed", 20, 20);
 
 pikachu.learnAttackSkill(lightning);
 bulbasaur.learnAttackSkill(poisonSeed);
 
+//The first argument to `attack` should be the index (or key) of the attack
 pikachu.attack(0, bulbasaur);
 bulbasaur.attack(0, pikachu);
-
 pikachu.showStatus();
 bulbasaur.showStatus();
-
+pikachu.attack(0, bulbasaur);
 pikachu.attack(0, bulbasaur);
 pikachu.attack(0, bulbasaur);
 pikachu.getMagic();
 pikachu.attack(0, bulbasaur);
-bulbasaur.attack(0, pikachu);
 bulbasaur.attack(0, pikachu);
